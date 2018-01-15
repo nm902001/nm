ab9247dde1ee1ce82b45ad44493e33f62499e9b7
nm902001 <35309619+nm902001@users.noreply.github.com>
2018-01-15 14:10:43 +0530

    Add files via upload

---
+++ first/src/first/First.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package first;
+
+/**
+ *
+ * @author nikhil
+ */
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class First extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Hello world!", 50, 25);
+    }
+}    
+
