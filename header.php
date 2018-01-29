ccb111983942814c3cc9d13ae6436b29717fb9bf
nm902001 <35309619+nm902001@users.noreply.github.com>
2018-01-30 00:07:32 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<head>
+<div class="box blue">
+<h2 style="font-family: Georgia; font-size: 300%; color: #FFFFFF;"><center><b>Student Feedback System</b></center></h2>
+</div>
+</head>
+
+
+<style type="text/css" >
+.box{ position:fixed; top: 0px; left: 0px; 
+	width:1366px;
+	height:80px;
+}
+.blue{
+	background: #3b5999;
+}
+</style>
